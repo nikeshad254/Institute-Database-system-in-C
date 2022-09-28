6127f5de1b55e5fde71efe7f3d177195b9616148
Nikesh <nikeshadhikari896@gmail.com>
2022-09-28 06:46:59 +0545

    file creation and template/format to data fetch and store made

---
+++ template.h
@@ -0,0 +1,47 @@
+// declaring all libaries we may need through out files
+#include<stdio.h>
+#include<string.h>
+#include<conio.h>
+#include <stdlib.h>
+
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
+// structure template
+// for institute
+struct institute{
+	int inst_id;
+	char name[20];
+	char pass[16];
+};
+// for student
+struct student{
+	int stu_id;
+	int roll_no;
+	char fname[20];
+	char lname[20];
+	int gender; // 1=male , 0=female ;;;
+	char dob[30];
+	int phone;
+	char email[40];
+	
+	// address info:
+	struct addres{
+		char per_prov[20];
+		char per_dist[20];
+		char per_street[20];
+		
+		char temp_prov[20];
+		char temp_dist[20];
+		char temp_street[20];
+	}address;
+	
+};
+// for reasult
+struct reasult{
+	float sub[5];
+	float per;
+};
