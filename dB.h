0db45d499172d593f521c774f2c406c8fa77ac79
Nikesh <nikeshadhikari896@gmail.com>
2022-09-29 07:28:04 +0545

    Dynamic pathing in DB & fetch admin code function made and implemented code verification in Main control

--- dB.h
+++ dB.h
@@ -1,1 +1,28 @@
 #include "template.h"
+
+//dynamic pathing
+char db[] = "all_dbs/";
+char inst_file[] = "institutes.txt";
+char admin_code[] = "admin_code.txt";
+char c_data[] = "all_dbs/c_datas/";
+
+//function declarartion
+int fetch_passcode();
+
+
+int fetch_passcode(){
+	char path[300];
+	int pass;
+	strcpy(path,db);
+	strcat(path,admin_code);
+	
+	FILE *fp;
+	fp = fopen(path,"r");
+	if(fp==NULL){
+		printf("\n\t...Error Ouccured...");
+		exit(0);
+	}
+	fscanf(fp,"%d",&pass);
+	fclose(fp);
+	return pass;
+}
--- mainControl.c
+++ mainControl.c
@@ -1,8 +1,9 @@
 #include "dB.h"
 
 void main(){
-	int opt;			//options declarations
-	int i, j;			// loop vars
+	int opt, chance;			//options declarations
+	int i, j;			// loop vars			
+	int admin_pass, prov_pass;		//password holder
 	do{
 		printf("\tWelcome to Institute Database\n...............................................\n");
 		printf("Who are you?\n1. User\n2. Admin\n3. Exit\n===> ");
@@ -13,7 +14,36 @@ void main(){
 			break;
 			
 			case 2: //for admin
-			
+				admin_pass = fetch_passcode();
+				chance = 0;
+				
+				do{//loop for admin code verification only
+					printf("admin is required to provide pass code:- ");
+					scanf("%d",&prov_pass);
+					
+					if(prov_pass==admin_pass){
+						
+						printf("admin login sucessfull!!");
+						break;	//ends password loop!!
+						
+					}else{
+						
+						chance++;
+						if(chance==3){
+							printf("\n no more tries -- Seems you are not admin!! \n\n BYE.......\n");
+							exit(0);
+						}else{
+							printf("\nincorrect admin pass code, try again(%d)\n\n", chance);	
+						}
+					}
+					
+					
+				}while(chance<=3);
+				
+				
+				
+				
+				
 			break;
 			
 			case 3:	//for exit
